25ea1f61506fd6b0a6581a6cc0028dd529e8b78b
sai manas goud <saimanas670@gmail.com>
2026-04-05 18:55:35 +0530

    fix api issue

--- src/main/java/com/renthome/renthome_backend/config/WebConfig.java
+++ src/main/java/com/renthome/renthome_backend/config/WebConfig.java
@@ -4,14 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")   // 🔥 THIS FIXES EVERYTHING
+                .allowedOriginPatterns("*")   // ✅ allow all vercel URLs
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
