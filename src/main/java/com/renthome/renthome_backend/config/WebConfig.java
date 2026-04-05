5c5810423a89d23caa9f23234858071c939c27c9
sai manas goud <saimanas670@gmail.com>
2026-04-05 18:50:15 +0530

    fix api issue

--- src/main/java/com/renthome/renthome_backend/config/WebConfig.java
+++ src/main/java/com/renthome/renthome_backend/config/WebConfig.java
@@ -4,18 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:5173",
-                    "https://renthome-frontend-5axg.vercel.app",
-                    "https://renthome-frontend-5axg-dn5q80ntd-saimanasgouds-projects.vercel.app"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOriginPatterns("*")   // 🔥 THIS FIXES EVERYTHING
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
